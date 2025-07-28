b43128cd9747b35f8e333a485790f56de378c02a
dcastica <danielorlandocas.jcol@masorange.es>
2025-07-28 08:35:29 -0500

    1

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ $mensaje = isset($_GET['error']) ? "Credenciales incorrectas" : "";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - TIMS</title>
+    <link rel="stylesheet" href="/src/css/index.css">
 </head>
 
 <body>
